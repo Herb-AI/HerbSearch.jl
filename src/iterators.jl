737a384b3a4d508fe8c474e10c8c5baa831e586f
jaapjong <J.deJong-18@student.tudelft.nl>
2023-01-31 17:01:03 +0100

    Fix indentation in iterator

--- src/iterators.jl
+++ src/iterators.jl
@@ -22,7 +22,7 @@ function Base.iterate(iter::ExpressionIterator)
     if isterminal(grammar, node)
         return (deepcopy(node), node)
     else
-        node, worked =  _next_state!(node, grammar, max_depth)
+        node, worked = _next_state!(node, grammar, max_depth)
         while !worked
             # increment root's rule
             rules = grammar[sym]
@@ -88,106 +88,106 @@ function _next_state!(node::RuleNode, grammar::Grammar, max_depth::Int)
 	elseif isterminal(grammar, node)
 	    # do nothing
 	    if iseval(grammar, node.ind) && (node._val === nothing)  # evaluate the rule
-		node._val = eval(grammar.rules[node.ind].args[2])
+			node._val = eval(grammar.rules[node.ind].args[2])
 	    end
 	    return (node, false) # cannot change leaves
 	else # !isterminal
 	    # if node is not terminal and doesn't have children, expand every child
 	    if isempty(node.children)  
-		if max_depth ≤ 1
-		    return (node,false) # cannot expand
-		end
-    
-		# build out the node
-		for c in child_types(grammar, node)
-    
-		    worked = false
-		    i = 0
-		    child = RuleNode(0)
-		    child_rules = grammar[c]
-		    while !worked && i < length(child_rules)
-			i += 1
-			child = RuleNode(child_rules[i])
-    
-			if iseval(grammar, child.ind) # if rule needs to be evaluated (_())
-			    child._val = eval(grammar.rules[child.ind].args[2])
+			if max_depth ≤ 1
+				return (node,false) # cannot expand
 			end
-			worked = true
-			if !isterminal(grammar, child)
-			    child, worked = _next_state!(child, grammar, max_depth-1)
+		
+			# build out the node
+			for c in child_types(grammar, node)
+		
+				worked = false
+				i = 0
+				child = RuleNode(0)
+				child_rules = grammar[c]
+				while !worked && i < length(child_rules)
+					i += 1
+					child = RuleNode(child_rules[i])
+			
+					if iseval(grammar, child.ind) # if rule needs to be evaluated (_())
+						child._val = eval(grammar.rules[child.ind].args[2])
+					end
+					worked = true
+					if !isterminal(grammar, child)
+						child, worked = _next_state!(child, grammar, max_depth-1)
+					end
+				end
+				if !worked
+					return (node, false) # did not work
+				end
+				push!(node.children, child)
 			end
-		    end
-		    if !worked
-			return (node, false) # did not work
-		    end
-		    push!(node.children, child)
-		end
-    
-		return (node, true)
+		
+			return (node, true)
 	    else # not empty
-		# make one change, starting with rightmost child
-		worked = false
-		child_index = length(node.children) + 1
-		while !worked && child_index > 1
-		    child_index -= 1
-		    child = node.children[child_index]
-    
-		    # this modifies the node if succesfull
-		    child, child_worked = _next_state!(child, grammar, max_depth-1)
-		    while !child_worked
-			child_type = return_type(grammar, child)
-			child_rules = grammar[child_type]
-			i = something(findfirst(isequal(child.ind), child_rules), 0)
-			if i < length(child_rules)
-			    child_worked = true
-			    child = RuleNode(child_rules[i+1])
-    
-			    # node needs to be evaluated
-			    if iseval(grammar, child.ind)
-				child._val = eval(grammar.rules[child.ind].args[2])
-			    end
-    
-			    if !isterminal(grammar, child)
+			# make one change, starting with rightmost child
+			worked = false
+			child_index = length(node.children) + 1
+			while !worked && child_index > 1
+				child_index -= 1
+				child = node.children[child_index]
+		
+				# this modifies the node if succesfull
 				child, child_worked = _next_state!(child, grammar, max_depth-1)
-			    end
-			    node.children[child_index] = child
-			else
-			    break
-			end
-		    end
-    
-		    if child_worked
-			worked = true
-    
-			# reset remaining children
-			for child_index2 in child_index+1 : length(node.children)
-			    c = child_types(grammar, node)[child_index2]
-			    worked = false
-			    i = 0
-			    child = RuleNode(0)
-			    child_rules = grammar[c]
-			    while !worked && i < length(child_rules)
-				i += 1
-				child = RuleNode(child_rules[i])
-    
-				if iseval(grammar, child.ind)
-				    child._val = eval(grammar.rules[child.ind].args[2])
+				while !child_worked
+					child_type = return_type(grammar, child)
+					child_rules = grammar[child_type]
+					i = something(findfirst(isequal(child.ind), child_rules), 0)
+					if i < length(child_rules)
+						child_worked = true
+						child = RuleNode(child_rules[i+1])
+			
+						# node needs to be evaluated
+						if iseval(grammar, child.ind)
+							child._val = eval(grammar.rules[child.ind].args[2])
+						end
+			
+						if !isterminal(grammar, child)
+							child, child_worked = _next_state!(child, grammar, max_depth-1)
+						end
+						node.children[child_index] = child
+					else
+						break
+					end
 				end
-    
-				worked = true
-				if !isterminal(grammar, child)
-				    child, worked = _next_state!(child, grammar, max_depth-1)
+		
+				if child_worked
+					worked = true
+		
+					# reset remaining children
+					for child_index2 in child_index+1 : length(node.children)
+						c = child_types(grammar, node)[child_index2]
+						worked = false
+						i = 0
+						child = RuleNode(0)
+						child_rules = grammar[c]
+						while !worked && i < length(child_rules)
+							i += 1
+							child = RuleNode(child_rules[i])
+				
+							if iseval(grammar, child.ind)
+								child._val = eval(grammar.rules[child.ind].args[2])
+							end
+				
+							worked = true
+							if !isterminal(grammar, child)
+								child, worked = _next_state!(child, grammar, max_depth-1)
+							end
+						end
+						if !worked
+							break
+						end
+						node.children[child_index2] = child
+					end
 				end
-			    end
-			    if !worked
-				break
-			    end
-			    node.children[child_index2] = child
 			end
-		    end
-		end
-    
-		return (node, worked)
+		
+			return (node, worked)
 	    end
 	end
 end
