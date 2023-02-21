f9fc7a131512f76cdf4aaf540f05a063c6b2d1af
Jaap de Jong <jaapdejong15@gmail.com>
2023-02-21 16:28:59 +0100

    Make the search procedure, BFS and DFS work with CSGs

--- src/search_procedure.jl
+++ src/search_procedure.jl
@@ -1,7 +1,7 @@
 """
 Searches the grammar up to the provided depth for a program that satisfies problem
 """
-function search(g::Grammars.Grammar, problem::Data.Problem, depth::Int, enumerator=ContextFreeEnumerator) :: Expr
+function search(g::Grammars.Grammar, problem::Data.Problem, depth::Int, enumerator=ExpressionIterator)
     symboltable :: SymbolTable = Grammars.SymbolTable(g)
 
     hypotheses = enumerator(g, depth, :Real)
@@ -16,4 +16,5 @@ function search(g::Grammars.Grammar, problem::Data.Problem, depth::Int, enumerat
             return expr
         end
     end
+    return nothing
 end
