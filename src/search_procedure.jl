c7903760bf3042ceea4c2f6267d87cb89de21260
Jaap de Jong <jaapdejong15@gmail.com>
2023-03-03 11:05:05 +0100

    Rename modules

--- src/search_procedure.jl
+++ src/search_procedure.jl
@@ -1,14 +1,14 @@
 """
 Searches the grammar up to the provided depth for a program that satisfies problem
 """
-function search(g::Grammars.Grammar, problem::Data.Problem, depth::Int, start::Symbol, enumerator=ExpressionIterator)::Any
+function search(g::Grammar, problem::Problem, depth::Int, start::Symbol, enumerator=ExpressionIterator)::Any
     symboltable :: SymbolTable = Grammars.SymbolTable(g)
 
     hypotheses = enumerator(g, depth, start)
 
     for h :: RuleNode ∈ hypotheses
         # Create expression from rulenode representation of AST
-        expr = Grammars.rulenode2expr(h, g)
+        expr = rulenode2expr(h, g)
 
         # Evaluate examples the examples.
         #  `evaluate examples` returns as soon as it has found the first example that doesn't work.
