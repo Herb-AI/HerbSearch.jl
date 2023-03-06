3519ccf909de9b7b588617d636b75c16f49f521d
Jaap de Jong <jaapdejong15@gmail.com>
2023-03-06 13:53:48 +0100

    Add stopping conditions to the search procedures

--- src/search_procedure.jl
+++ src/search_procedure.jl
@@ -1,27 +1,48 @@
 """
-Searches the grammar up to the provided depth for a program that satisfies problem.
-The evaluator should be a function that takes a SymbolTable, expression and a dictionary with 
-    input variable assignments and returns the output of the expression.
+Searches the grammar for the program that satisfies the maximum number of examples in the problem.
+    
+        - g                 - The grammar that defines the search space
+        - problem           - The problem definition with IO examples
+        - start             - The start symbol in the grammar
+        - evaluator         - The evaluation function. Takes a SymbolTable, expression and a dictionary with 
+                              input variable assignments and returns the output of the expression.
+        - enumerator        - A constructor for the enumerator that should be used in the search
+        - max_depth         - The maximum depth of the search
+        - max_time          - The maximum time allowed for the search in seconds
+        - max_enumerations  - The maximum number of programs to enumerate and test
+    Returns the optimal program once it has been found, or nothing otherwise.
 """
-function search(g::Grammar, problem::Problem, depth::Int, start::Symbol, evaluator=test_with_input, enumerator=get_bfs_enumerator)::Any
+function search(
+        g::Grammar, 
+        problem::Problem, 
+        start::Symbol; 
+        evaluator=test_with_input, 
+        enumerator=get_bfs_enumerator,
+        max_depth::Union{Int, Nothing}=nothing,
+        max_time::Union{Int, Nothing}=nothing,
+        max_enumerations::Union{Int, Nothing}=nothing
+    )::Any
+
+    start_time = time()
+    check_time = max_time !== nothing
+    check_enumerations = max_enumerations !== nothing
     symboltable :: SymbolTable = SymbolTable(g)
 
-    hypotheses = enumerator(g, depth, start)
+    hypotheses = enumerator(g, max_depth ≡ nothing ? typemax(Int) : max_depth , start)
 
-    for h :: RuleNode ∈ hypotheses
+    for (i, h) ∈ enumerate(hypotheses)
         # Create expression from rulenode representation of AST
         expr = rulenode2expr(h, g)
 
-        # Evaluate the examples.
-        satisfied = true
-        for example ∈ filter(e -> e isa IOExample, problem.examples)
-            if example.out ≠ evaluator(symboltable, expr, example.in)
-                satisfied = false
-                break
-            end
-        end
-        if satisfied
+        # Evaluate the examples. 
+        # `all` shortcircuits, so not every example will be evaluated in every iteration. 
+        if all(example.out == evaluator(symboltable, expr, example.in) for example ∈ problem.examples)
             return expr
+        end
+
+        # Check stopping conditions
+        if check_enumerations && i > max_enumerations || check_time && time() - start_time > max_time
+            return nothing
         end
     end
     return nothing
@@ -29,28 +50,58 @@ end
 
 
 """
-Searches the grammar up to the provided depth for the program that satisfies the maximum number of examples in the problem.
+Searches the grammar for the program that satisfies the maximum number of examples in the problem.
 The evaluator should be a function that takes a SymbolTable, expression and a dictionary with 
     input variable assignments and returns the output of the expression.
-Returns a tuple with the found program and a number between 0 and 1 indicating the fraction of examples it satisfies. 
+
+    - g                 - The grammar that defines the search space
+    - problem           - The problem definition with IO examples
+    - start             - The start symbol in the grammar
+    - evaluator         - The evaluation function. Takes a SymbolTable, expression and a dictionary with 
+                          input variable assignments and returns the output of the expression.
+    - enumerator        - A constructor for the enumerator that should be used in the search
+    - max_depth         - The maximum depth of the search
+    - max_time          - The maximum time allowed for the search in seconds
+    - max_enumerations  - The maximum number of programs to enumerate and test
+Returns a tuple with the best found program so far and a number between 0 and 1 indicating the fraction of examples it satisfies. 
+Can be considerably slower than `search` due to having to evaluate each expression on each example.
 """
-function search_best(g::Grammar, problem::Problem, depth::Int, start::Symbol, evaluator=test_with_input, enumerator=get_bfs_enumerator)::Tuple{Any, Real}
+function search_best(
+        g::Grammar, 
+        problem::Problem, 
+        start::Symbol;
+        evaluator=test_with_input, 
+        enumerator=get_bfs_enumerator,
+        max_depth::Union{Int, Nothing}=nothing,
+        max_time::Union{Int, Nothing}=nothing,
+        max_enumerations::Union{Int, Nothing}=nothing
+    )::Tuple{Any, Real}
+
+    start_time = time()
+    check_time = max_time !== nothing
+    check_enumerations = max_enumerations !== nothing
     symboltable :: SymbolTable = SymbolTable(g)
 
-    hypotheses = enumerator(g, depth, start)
+    hypotheses = enumerator(g, max_depth ≡ nothing ? typemax(Int) : max_depth , start)
 
     best_num_passing_examples = -1
     best_program = nothing
-    for h :: RuleNode ∈ hypotheses
+    for (i, h) ∈ enumerate(hypotheses)
         # Create expression from rulenode representation of AST
         expr = rulenode2expr(h, g)
 
+        # Evaluate
         passing_examples = count(evaluator(symboltable, expr, example.in) == example.out for example ∈ problem.examples)
         if passing_examples == length(problem.examples)
-            return expr
+            return expr, 1
         elseif passing_examples > best_num_passing_examples
             best_num_passing_examples = passing_examples
             best_program = expr
+        end
+
+        # Check stopping conditions
+        if check_enumerations && i > max_enumerations || check_time && time() - start_time > max_time
+            return best_program, best_num_passing_examples / length(problem.examples)
         end
     end
     return best_program, best_num_passing_examples / length(problem.examples)
--- test/test_search_procedure.jl
+++ test/test_search_procedure.jl
@@ -1,24 +1,40 @@
 @testset verbose=true "Search procedure" begin
     g₁ = @cfgrammar begin
-        Number = |(0:4)
+        Number = |(1:5)
         Number = x
         Number = Number + Number
         Number = Number * Number
+        Number = Number - Number
     end
 
     @testset "Search" begin
         problem = Problem([IOExample(Dict(:x => x), 3x+2) for x ∈ 1:5], "")
-        solution = search(g₁, problem, 3, :Number)
+        solution = search(g₁, problem, :Number, max_depth=3)
 
         @test test_with_input(SymbolTable(g₁), solution, Dict(:x => 6)) == 3*6+2
     end
 
+    @testset "Search max_enumerations stopping condition" begin
+        problem = Problem([IOExample(Dict(:x => x), 3x+2) for x ∈ 1:5], "")
+        solution = search(g₁, problem, :Number, max_enumerations=5)
+
+        @test solution ≡ nothing
+    end
+
     @testset "Best search" begin
         problem = Problem(push!([IOExample(Dict(:x => x), 3x+2) for x ∈ 1:5], IOExample(Dict(:x => 5), 15)), "")
-        solution, correctness = search_best(g₁, problem, 3, :Number)
+        solution, correctness = search_best(g₁, problem, :Number, max_depth=3)
 
         @test correctness ≈ 5/6
         @test test_with_input(SymbolTable(g₁), solution, Dict(:x => 6)) == 3*6+2
 
+    end
+
+    @testset "Search_best max_enumerations stopping condition" begin
+        problem = Problem([IOExample(Dict(:x => x), 3x-2) for x ∈ 1:5], "")
+        solution, correctness = search_best(g₁, problem, :Number, max_enumerations=5)
+
+        @test solution == 1 || solution == 4
+        @test correctness == 1/5 
     end
 end
