0210981c42865bfc2fbdd57839be55a67e793ac3
Jaap de Jong <jaapdejong15@gmail.com>
2023-03-06 14:05:21 +0100

    Make tests run faster by making grammar smaller

--- test/test_search_procedure.jl
+++ test/test_search_procedure.jl
@@ -1,40 +1,39 @@
 @testset verbose=true "Search procedure" begin
     g₁ = @cfgrammar begin
-        Number = |(1:5)
+        Number = |(1:2)
         Number = x
         Number = Number + Number
         Number = Number * Number
-        Number = Number - Number
     end
 
     @testset "Search" begin
-        problem = Problem([IOExample(Dict(:x => x), 3x+2) for x ∈ 1:5], "")
+        problem = Problem([IOExample(Dict(:x => x), 2x+1) for x ∈ 1:5], "")
         solution = search(g₁, problem, :Number, max_depth=3)
 
-        @test test_with_input(SymbolTable(g₁), solution, Dict(:x => 6)) == 3*6+2
+        @test test_with_input(SymbolTable(g₁), solution, Dict(:x => 6)) == 2*6+1
     end
 
     @testset "Search max_enumerations stopping condition" begin
-        problem = Problem([IOExample(Dict(:x => x), 3x+2) for x ∈ 1:5], "")
+        problem = Problem([IOExample(Dict(:x => x), 2x+1) for x ∈ 1:5], "")
         solution = search(g₁, problem, :Number, max_enumerations=5)
 
         @test solution ≡ nothing
     end
 
     @testset "Best search" begin
-        problem = Problem(push!([IOExample(Dict(:x => x), 3x+2) for x ∈ 1:5], IOExample(Dict(:x => 5), 15)), "")
+        problem = Problem(push!([IOExample(Dict(:x => x), 2x+1) for x ∈ 1:5], IOExample(Dict(:x => 5), 15)), "")
         solution, correctness = search_best(g₁, problem, :Number, max_depth=3)
 
         @test correctness ≈ 5/6
-        @test test_with_input(SymbolTable(g₁), solution, Dict(:x => 6)) == 3*6+2
+        @test test_with_input(SymbolTable(g₁), solution, Dict(:x => 6)) == 2*6+1
 
     end
 
     @testset "Search_best max_enumerations stopping condition" begin
-        problem = Problem([IOExample(Dict(:x => x), 3x-2) for x ∈ 1:5], "")
-        solution, correctness = search_best(g₁, problem, :Number, max_enumerations=5)
+        problem = Problem([IOExample(Dict(:x => x), 2x-1) for x ∈ 1:5], "")
+        solution, correctness = search_best(g₁, problem, :Number, max_enumerations=2)
 
-        @test solution == 1 || solution == 4
+        @test solution == 1
         @test correctness == 1/5 
     end
 end
