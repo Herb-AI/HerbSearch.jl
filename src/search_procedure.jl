383f80957d7ea1a700dd0935a80cbca7159f751e
Jaap de Jong <jaapdejong15@gmail.com>
2023-03-06 12:12:37 +0100

    Add custom evaluator to the search procedure

--- src/search_procedure.jl
+++ src/search_procedure.jl
@@ -1,7 +1,9 @@
 """
-Searches the grammar up to the provided depth for a program that satisfies problem
+Searches the grammar up to the provided depth for a program that satisfies problem.
+The evaluator should be a function that takes a SymbolTable, expression and a dictionary with 
+    input variable assignments and returns the output of the expression.
 """
-function search(g::Grammar, problem::Problem, depth::Int, start::Symbol, enumerator=ExpressionIterator)::Any
+function search(g::Grammar, problem::Problem, depth::Int, start::Symbol, evaluator=test_with_input, enumerator=get_bfs_enumerator)::Any
     symboltable :: SymbolTable = SymbolTable(g)
 
     hypotheses = enumerator(g, depth, start)
@@ -10,9 +12,15 @@ function search(g::Grammar, problem::Problem, depth::Int, start::Symbol, enumera
         # Create expression from rulenode representation of AST
         expr = rulenode2expr(h, g)
 
-        # Evaluate examples the examples.
-        #  `test_examples` returns as soon as it has found the first example that doesn't work.
-        if test_examples(symboltable, expr, problem.examples)
+        # Evaluate the examples.
+        satisfied = true
+        for example ∈ filter(e -> e isa IOExample, problem.examples)
+            if example.out ≠ evaluator(symboltable, expr, example.in)
+                satisfied = false
+                break
+            end
+        end
+        if satisfied
             return expr
         end
     end
