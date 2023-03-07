466d6f6bee7120a503225460a83776c3da26d202
Jaap de Jong <jaapdejong15@gmail.com>
2023-03-07 08:57:19 +0100

    Add custom error function to search_best

--- src/search_procedure.jl
+++ src/search_procedure.jl
@@ -16,8 +16,8 @@ function search(
         g::Grammar, 
         problem::Problem, 
         start::Symbol; 
-        evaluator=test_with_input, 
-        enumerator=get_bfs_enumerator,
+        evaluator::Function=test_with_input, 
+        enumerator::Function=get_bfs_enumerator,
         max_depth::Union{Int, Nothing}=nothing,
         max_time::Union{Int, Nothing}=nothing,
         max_enumerations::Union{Int, Nothing}=nothing
@@ -50,6 +50,18 @@ end
 
 
 """
+Default error function for `search_best`.
+    
+    - old_error         - The existing total error
+    - output            - The actual output of the evaluator
+    - expected_output   - The expected output for the example
+
+The default function returns `0` if the outputs match and `1` otherwise.
+"""
+default_error_function(old_error, output, expected_output) = old_error + (output == expected_output ? 0 : 1)
+
+
+"""
 Searches the grammar for the program that satisfies the maximum number of examples in the problem.
 The evaluator should be a function that takes a SymbolTable, expression and a dictionary with 
     input variable assignments and returns the output of the expression.
@@ -60,18 +72,21 @@ The evaluator should be a function that takes a SymbolTable, expression and a di
     - evaluator         - The evaluation function. Takes a SymbolTable, expression and a dictionary with 
                           input variable assignments and returns the output of the expression.
     - enumerator        - A constructor for the enumerator that should be used in the search
+    - error_function    - The error function. Takes the existing total error, the actual output of the evaluator 
+                          and the expected value for the example.
     - max_depth         - The maximum depth of the search
     - max_time          - The maximum time allowed for the search in seconds
     - max_enumerations  - The maximum number of programs to enumerate and test
-Returns a tuple with the best found program so far and a number between 0 and 1 indicating the fraction of examples it satisfies. 
+Returns a tuple with the best found program so far and the error. 
 Can be considerably slower than `search` due to having to evaluate each expression on each example.
 """
 function search_best(
         g::Grammar, 
         problem::Problem, 
         start::Symbol;
-        evaluator=test_with_input, 
-        enumerator=get_bfs_enumerator,
+        evaluator::Function=test_with_input, 
+        enumerator::Function=get_bfs_enumerator,
+        error_function::Function=default_error_function,
         max_depth::Union{Int, Nothing}=nothing,
         max_time::Union{Int, Nothing}=nothing,
         max_enumerations::Union{Int, Nothing}=nothing
@@ -84,35 +99,35 @@ function search_best(
 
     hypotheses = enumerator(g, max_depth ≡ nothing ? typemax(Int) : max_depth , start)
 
-    best_num_passing_examples = -1
+    best_error = typemax(Real)
     best_program = nothing
     for (i, h) ∈ enumerate(hypotheses)
         # Create expression from rulenode representation of AST
         expr = rulenode2expr(h, g)
 
         # Evaluate the expression on the examples
-        passing_examples = 0
-        for (j, example) ∈ enumerate(problem.examples)
-            passing_examples += evaluator(symboltable, expr, example.in) == example.out ? 1 : 0
+        total_error = 0
+        for example ∈ problem.examples
+            total_error = error_function(total_error, evaluator(symboltable, expr, example.in), example.out)
 
             # Check if we can still improve the best program found so far
-            if passing_examples + length(problem.examples) - j ≤ best_num_passing_examples
+            if sum_of_error ≥ best_error
                 break
             end
         end
 
-        if passing_examples == length(problem.examples)
-            return expr, 1
-        elseif passing_examples > best_num_passing_examples
+        if total_error == 0
+            return expr, 0
+        elseif total_error < best_error
             # Update the best found example so far
-            best_num_passing_examples = passing_examples
+            best_error = total_error
             best_program = expr
         end
 
         # Check stopping conditions
         if check_enumerations && i > max_enumerations || check_time && time() - start_time > max_time
-            return best_program, best_num_passing_examples / length(problem.examples)
+            return best_program, best_error
         end
     end
-    return best_program, best_num_passing_examples / length(problem.examples)
+    return best_program, best_error
 end
