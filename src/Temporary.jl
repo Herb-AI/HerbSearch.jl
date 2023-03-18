16b3ac79db7d5cbe7e584c3d377f54b7e2df2c35
Piotr Cichoń <P.P.Cichon-1@student.tudelft.nl>
2023-03-18 20:22:44 +0100

    Add temporary file to run search algorithms

---
+++ src/Temporary.jl
@@ -0,0 +1,21 @@
+include("../../src/Herb.jl")
+using .Herb
+using Logging
+using StatsBase
+
+grammar = Herb.HerbGrammar.@cfgrammar begin 
+    C = |(1:5)
+    X = |(1:5)
+    X = C * X
+    X = X + X 
+    X = X * X
+    X = x
+end 
+
+# disable_logging(LogLevel(1))
+examples_hard = [Herb.HerbData.IOExample(Dict(:x => x), x * (x + 5) + 1) for x ∈ 1:10]
+problem = Herb.HerbData.Problem(examples_hard, "example")
+
+enumerator = HerbSearch.get_vlsn_enumerator(grammar, examples_hard, 5, :X, HerbSearch.mean_squared_error)
+@time work = Herb.HerbSearch.search_it(grammar, problem, enumerator)
+println(work)
