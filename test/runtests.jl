d4eed9da1656f2b2ae89e33fb444327c25913081
Nicolae Filat <N.Filat@student.tudelft.nl>
2023-06-30 17:27:12 +0200

    Removed full package names in tests

---
+++ test/runtests.jl
@@ -0,0 +1,15 @@
+using HerbCore
+using HerbSearch
+using HerbGrammar
+using HerbEvaluation
+using HerbConstraints
+using HerbData
+using Test
+
+@testset "HerbSearch.jl" verbose=true begin
+    #include("realistic_search_tests.jl")
+    include("test_stochastic_algorithms.jl")
+    include("test_context_sensitive_iterators.jl")
+    include("test_search_procedure.jl")        
+    include("test_context_free_iterators.jl")
+end
