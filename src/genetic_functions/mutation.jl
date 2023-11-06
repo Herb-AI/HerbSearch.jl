ef6decc15280245d63435bdd02525fe04beddc99
Nicolae Filat <nicolae.filat@gmail.com>
2023-11-06 17:32:34 +0100

    Fix mutate_random! to take into account the node type when sampling

--- src/genetic_functions/mutation.jl
+++ src/genetic_functions/mutation.jl
@@ -1,11 +1,14 @@
 """
-    mutate_random!(program::RuleNode, grammar::Grammar, max_depth::Int64 = 2)
+    mutate_random!(program::RuleNode, grammar::Grammar)
 
 Mutates the given program by inserting a randomly generated sub-program at a random location.
 """
-function mutate_random!(program::RuleNode, grammar::Grammar, max_depth::Int64 = 2)
+function mutate_random!(program::RuleNode, grammar::Grammar)
     node_location::NodeLoc = sample(NodeLoc, program)
-    random_program = rand(RuleNode, grammar,  max_depth)
+    subprogram = get(program, node_location)
+    symbol = return_type(grammar, subprogram)
+
+    random_program = rand(RuleNode, grammar, symbol)
     insert!(program, node_location, random_program)
 end
 
