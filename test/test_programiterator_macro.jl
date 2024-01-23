c3f9a8a210f479c71a9f1857eaf48a707b606f6d
Luka Janjić <luka.janjic@gmail.com>
2024-01-23 14:07:42 +0100

    add tests

--- test/test_programiterator_macro.jl
+++ test/test_programiterator_macro.jl
@@ -47,10 +47,10 @@
         @test AnotherIterator <: IteratorFamily
     end
 
-    @testset "with default constructor" begin
+    @testset "with inner constructor" begin
         @programiterator mutable DefConstrIterator(
             function DefConstrIterator()
-                g = @csgrammar R = x
+                g = @csgrammar begin R = x end
                 new(g, :R, 5, 5, 5, 5)
             end
         )
@@ -58,5 +58,54 @@
         it = DefConstrIterator()
         
         @test it.max_enumerations == me && it.max_depth == md
-    end 
+    end
+
+    @testset "with default values" begin
+        @programiterator DefValIterator(
+            a::Int=5,
+            b=nothing
+        )
+
+        it = DefValIterator(g, :R)
+
+        @test it.a == 5 && isnothing(it.b)
+        @test it.max_depth == typemax(Int)
+
+        it = DefValIterator(g, :R, max_depth=5)
+
+        @test it.max_depth == 5
+    end
+
+    @testset "all together" begin
+        @programiterator mutable ComplicatedIterator(
+            intfield::Int,
+            deffield=nothing,
+            function ComplicatedIterator(g, s, md, ms, mt, me, i, d) 
+                new(g, s, md, ms, mt, me, i, d)
+            end,
+            function ComplicatedIterator()
+                let g = @csgrammar begin
+                    R = x
+                    R = 1 | 2
+                end
+                    new(g, :R, 1, 2, 3, 4, 5, 6)
+                end
+            end
+        )
+
+        it = ComplicatedIterator()
+
+        @test length(it.grammar.rules) == 3
+        @test it.sym == :R
+        @test it.max_depth == 1
+        @test it.intfield == 5
+        @test it.deffield == 6
+
+        it = ComplicatedIterator(g, :S, 5; max_depth=10)
+
+        @test it.max_depth == 10
+        @test length(it.grammar.rules) == 1
+        @test it.sym == :S
+        @test isnothing(it.deffield)
+    end
 end
