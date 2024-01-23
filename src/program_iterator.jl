a57b65c95ddc549c2aebb8d83eebce8ed6998ee7
Luka Janjić <luka.janjic@gmail.com>
2024-01-23 14:18:58 +0100

    add to the docstring

--- src/program_iterator.jl
+++ src/program_iterator.jl
@@ -33,7 +33,8 @@ Syntax accepted by the macro is as follows (anything enclosed in square brackets
 Note that the macro emits an assertion that the `SupertypeIterator` 
 is a subtype of `ProgramIterator` which otherwise throws an ArgumentError.
 If no supertype is given, the new iterator extends `ProgramIterator` directly.
-Each <argᵢ> may be any expression valid in a struct declaration, and they must be comma separated.
+Each <argᵢ> may be (almost) any expression valid in a struct declaration, and they must be comma separated.
+One known exception is that an inner constructor must always be given using the extended `function <name>(...) ... end` syntax.
 The `mutable` keyword determines whether the declared struct is mutable.
 """
 macro programiterator(mut, ex)
