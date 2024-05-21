2acbace64edecc2f8e5ed938e4ca90fdcd44d5de
AlpiGun5 <alperen.tf2@gmail.com>
2024-05-21 19:45:36 +0200

    Refactor example to run

--- test/test_frangel/test_frangel_end_to_end.jl
+++ test/test_frangel/test_frangel_end_to_end.jl
@@ -11,12 +11,14 @@ end
     config = FrAngelConfig(generation = FrAngelConfigGeneration(use_fragments_chance = 0, use_angelic_conditions_chance = 0))
     angelic_conditions = AbstractVector{Union{Nothing, Int64}}([nothing for rule in g.rules])
     
-    # @time begin 
-    #     iterator = FrAngelRandomIterator(g, :Num, max_depth=10)
-    #     solution = frangel(spec, config, angelic_conditions, iterator) 
-    # end
-    # program = rulenode2expr(solution, g) # should yield 2*6 +1 
-    # println(program)
+    @time begin 
+        rules_min = rules_minsize(g)
+        symbol_min = symbols_minsize(g, rules_min)
+        iterator = FrAngelRandomIterator(g, :Num, rules_min, symbol_min, max_depth = 10)
+        solution = frangel(spec, config, angelic_conditions, iterator, rules_min, symbol_min) 
+    end
+    program = rulenode2expr(solution, g) # should yield 2*6 +1 
+    println(program)
 
     @time begin 
         iterator = BFSIterator(g, :Num, max_depth=10)
