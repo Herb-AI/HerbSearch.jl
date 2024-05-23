41c4c77a2008b52c22af27fcf061f865f8664739
AlpiGun5 <alperen.tf2@gmail.com>
2024-05-23 15:33:29 +0200

    Add an end-to-end test

--- test/test_frangel/test_frangel_end_to_end.jl
+++ test/test_frangel/test_frangel_end_to_end.jl
@@ -9,73 +9,57 @@ end
     spec = [IOExample(Dict(:x => x), 2x) for x ∈ 1:5]
     problem = Problem(spec)
     config = FrAngelConfig(verbose_level = 20, generation = FrAngelConfigGeneration(use_fragments_chance = 0.5, use_angelic_conditions_chance = 0, max_size=20))
-    angelic_conditions = AbstractVector{Union{Nothing, Int64}}([nothing for rule in g.rules])
     rules_min = rules_minsize(g)
     symbol_min = symbols_minsize(g, rules_min)
-    base_g = deepcopy(g)
 
     @time begin     
     # @time @profview begin     
-        iterator = FrAngelRandomIterator(g, :Num, rules_min, symbol_min, max_depth = config.generation.max_size)
-        solution = frangel(spec, config, angelic_conditions, iterator, rules_min, symbol_min) 
+        iterator = FrAngelRandomIterator(deepcopy(g), :Num, rules_min, symbol_min, max_depth = config.generation.max_size)
+        solution = frangel(spec, config, AbstractVector{Union{Nothing, Int64}}([nothing for rule in g.rules]), iterator, rules_min, symbol_min) 
     end
-    program = rulenode2expr(solution, g) # should yield 2*6 +1 
+    program = rulenode2expr(solution, g)
     println(program)
 
     @time begin 
-        iterator = BFSIterator(base_g, :Num, max_depth=10)
+        iterator = BFSIterator(g, :Num, max_depth=10)
         solution, flag = synth(problem, iterator)
     end
-    program = rulenode2expr(solution, base_g) # should yield 2*6 +1 
+    program = rulenode2expr(solution, g)
     println(program)
 end
 
-function buildProgrammingProblemGrammar(
-    input_parameters::AbstractVector{Tuple{Symbol, Symbol}}, 
-    return_type::Symbol,
-    intermediate_variables_count::Int=0
-)::ContextSensitiveGrammar
-    base_grammar = @cfgrammar begin
-        
-        Program = (VariableDefintion ; Statement ; Return) | (Statement ; Return) | Return
-        VariableDefintion = ListVariable = List
+@testset "basic_example_2" begin
+    spec = [
+        IOExample(Dict(:x => 1), 2)
+        IOExample(Dict(:x => 2), 3)
+        IOExample(Dict(:x => 3), 3)
+        IOExample(Dict(:x => 4), 5)
+        IOExample(Dict(:x => 5), 6)
+        IOExample(Dict(:x => 6), 7)
+    ]
+    problem = Problem(spec)
+    config = FrAngelConfig(verbose_level = 0, generation = FrAngelConfigGeneration(use_fragments_chance = 0.5, use_angelic_conditions_chance = 0, max_size=20))
+    angelic_conditions = AbstractVector{Union{Nothing, Int64}}([nothing for rule in g.rules])
+    rules_min = rules_minsize(g)
+    symbol_min = symbols_minsize(g, rules_min)
 
-        Statement = (Statement ; Statement)
-        Statement = (
-            i = 0;
-            while i < Num
-                InnerStatement
-                i = i + 1
-            end)
-        Statement = (if Bool Statement end)
-        Statement = push!(ListVariable, Num)
-
-        InnerNum = Num | i | (InnerNum + InnerNum)
-        InnerStatement = push!(ListVariable, InnerNum)
-        
-        Num = |(0:9) | (Num + Num) | (Num - Num)
-        Num = getindex(ListVariable, Num)
-
-        Bool = true | false
-
-        List = [] | ListVariable
-
-        ListVariable = list
+    @time begin     
+    # @time @profview begin     
+        iterator = FrAngelRandomIterator(deepcopy(g), :Num, rules_min, symbol_min, max_depth = config.generation.max_size)
+        solution = frangel(spec, config, angelic_conditions, iterator, rules_min, symbol_min) 
     end
+    program = rulenode2expr(solution, g)
+    println(program)
 
-    # add return type constraint
-    add_rule!(base_grammar, :(Return = return $return_type))
-
-    # add input parameters constraints
-    for input_parameter in input_parameters 
-        add_rule!(base_grammar, :($(input_parameter[2]) = $(input_parameter[1])))
+    @time begin 
+        iterator = BFSIterator(g, :Num, max_depth=10)
+        solution, flag = synth(problem, iterator)
     end
-
-    # what about order constrains for while loops?
-    # what about order constrains for variables
-
-    base_grammar
+    program = rulenode2expr(solution, g)
+    println(program)
 end
+
+
 
 @testset "getRange" begin
     grammar = buildProgrammingProblemGrammar([(:endd, :Num), (:start, :Num)], :List)
@@ -91,7 +75,7 @@ end
     angelic_conditions = AbstractVector{Union{Nothing, Int64}}([nothing for rule in grammar.rules])
     angelic_conditions[6] = 1
     angelic_conditions[7] = 1
-    config = FrAngelConfig(max_time = 80, generation = FrAngelConfigGeneration(use_fragments_chance = Float16(0.5), use_angelic_conditions_chance = 0))
+    config = FrAngelConfig(max_time = 40, generation = FrAngelConfigGeneration(use_fragments_chance = Float16(0.5), use_angelic_conditions_chance = 0))
 
     rules_min = rules_minsize(grammar)
     symbol_min = symbols_minsize(grammar, rules_min)
@@ -102,10 +86,10 @@ end
     program = rulenode2expr(solution, grammar) 
     println(program)
 
-    @time begin 
-        iterator = BFSIterator(b_grammar, :Num, max_depth=40)
-        solution, flag = synth(problem, iterator)
-    end
-    program = rulenode2expr(solution, b_grammar) # should yield 2*6 +1 
-    println(program)
+    # @time begin 
+    #     iterator = BFSIterator(b_grammar, :Program, max_depth=40)
+    #     solution, flag = synth(problem, iterator)
+    # end
+    # program = rulenode2expr(solution, b_grammar) # should yield 2*6 +1 
+    # println(program)
 end
