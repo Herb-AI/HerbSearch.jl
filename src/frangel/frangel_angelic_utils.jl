3c7dfa3d0674f5c554f3123b6a8d871939f4e70a
AlpiGun5 <alperen.tf2@gmail.com>
2024-05-29 23:57:20 +0200

    Update config to store rulenode over index

--- src/frangel/frangel_angelic_utils.jl
+++ src/frangel/frangel_angelic_utils.jl
@@ -113,7 +113,7 @@ end
 
 """
     execute_angelic_on_input(symboltable::SymbolTable, program::RuleNode, grammar::AbstractGrammar, input::Dict{Symbol,Any}, 
-        expected_output::Any, angelic_rulenode_idx::Int, max_attempts::Int, angelic_conditions::Dict{UInt16,UInt8})::Bool
+        expected_output::Any, angelic_rulenode::RuleNode, max_attempts::Int, angelic_conditions::Dict{UInt16,UInt8})::Bool
 
 Run test case `input` on `program` containing angelic conditions. This is done by optimistically evaluating the program, by generating different code paths
     and checking if any of them make the program pass the test case.
@@ -124,7 +124,7 @@ Run test case `input` on `program` containing angelic conditions. This is done b
 - `grammar`: The grammar rules of the program.
 - `input`: A dictionary where each key is a symbol used in the expression, and the value is the corresponding value to be used in the expression's evaluation.
 - `expected_output`: The expected output of the program.
-- `angelic_rulenode_idx`: The index of the anglelic rulenode. It is used to replace angelified conditions.
+- `angelic_rulenode`: The angelic rulenode. It is used to replace angelified conditions.
 - `max_attempts`: The maximum number of attempts before assuming the angelic program cannot be resolved.
 - `angelic_conditions`: A dictionary mapping indices of angelic condition candidates, to the child index that may be changed.
 
@@ -138,14 +138,14 @@ function execute_angelic_on_input(
     grammar::AbstractGrammar,
     input::Dict{Symbol,Any},
     expected_output::Any,
-    angelic_rulenode_idx::Int,
+    angelic_rulenode::RuleNode,
     max_attempts::Int,
     angelic_conditions::Dict{UInt16,UInt8}
 )::Bool
     num_true, attempts = 0, 0
     # We check traversed code paths by prefix -> trie is efficient for this
     visited = BitTrie()
-    expr = create_angelic_expression(program, grammar, angelic_rulenode_idx, angelic_conditions)
+    expr = create_angelic_expression(program, grammar, angelic_rulenode, angelic_conditions)
     while num_true < max_attempts
         code_paths = Vector{BitVector}()
         get_code_paths!(num_true, BitVector(), visited, code_paths, max_attempts - attempts)
@@ -209,14 +209,14 @@ function get_code_paths!(
 end
 
 """
-    create_angelic_expression(program::RuleNode, grammar::AbstractGrammar, angelic_rulenode_idx::Int, angelic_conditions::Dict{UInt16,UInt8})
+    create_angelic_expression(program::RuleNode, grammar::AbstractGrammar, angelic_rulenode::RuleNode, angelic_conditions::Dict{UInt16,UInt8})
 
-Create an angelic expression, i.e. replace all remaining holes with truthy trees so that the tree can be parsed and executed.
+Create an angelic expression, i.e. replace all remaining holes with angelic rulenode trees so that the tree can be parsed and executed.
 
 # Arguments
 - `program`: The program to turn into an angelic expression.
 - `grammar`: The grammar rules of the program.
-- `angelic_rulenode_idx`: The index of the anglelic rulenode. It is used to replace angelified conditions.
+- `angelic_rulenode`: The angelic rulenode. It is used to replace angelified conditions.
 - `angelic_conditions`: A dictionary mapping indices of angelic condition candidates, to the child index that may be changed.
 
 # Returns
@@ -226,7 +226,7 @@ The generated angelic expression.
 function create_angelic_expression(
     program::RuleNode,
     grammar::AbstractGrammar,
-    angelic_rulenode_idx::Int,
+    angelic_rulenode::RuleNode,
     angelic_conditions::Dict{UInt16,UInt8}
 )::Expr
     new_program = deepcopy(program)
@@ -238,7 +238,7 @@ function create_angelic_expression(
         angelic_idx = get(angelic_conditions, node.ind, -1)
         for (child_index, child) in enumerate(node.children)
             if angelic_idx == child_index && child isa AbstractHole
-                node.children[child_index] = RuleNode(angelic_rulenode_idx)
+                node.children[child_index] = angelic_rulenode
             else
                 enqueue!(queue, child)
             end
--- test/test_frangel/test_frangel_end_to_end.jl
+++ test/test_frangel/test_frangel_end_to_end.jl
@@ -11,28 +11,28 @@ g = @cfgrammar begin
     )
 end
 
-@testset "basic_example_fragments_only" begin
-    spec = [IOExample(Dict(:x => x), 3x) for x ∈ 1:5]
-    problem = Problem(spec)
-    rules_min = rules_minsize(g)
-    symbol_min = symbols_minsize(g, rules_min)
+# @testset "basic_example_fragments_only" begin
+#     spec = [IOExample(Dict(:x => x), 3x) for x ∈ 1:5]
+#     problem = Problem(spec)
+#     rules_min = rules_minsize(g)
+#     symbol_min = symbols_minsize(g, rules_min)
 
-    config = FrAngelConfig(verbose_level=0, generation=FrAngelConfigGeneration(use_fragments_chance=0.5, use_angelic_conditions_chance=0, max_size=20))
-    @time begin
-        # @time @profview begin     
-        iterator = FrAngelRandomIterator(deepcopy(g), :Num, rules_min, symbol_min, max_depth=config.generation.max_size)
-        solution = frangel(spec, config, Dict{UInt16, UInt8}(), iterator, rules_min, symbol_min)
-    end
-    program = rulenode2expr(solution, g)
-    println(program)
+#     config = FrAngelConfig(verbose_level=0, generation=FrAngelConfigGeneration(use_fragments_chance=0.5, use_angelic_conditions_chance=0, max_size=20))
+#     @time begin
+#         # @time @profview begin     
+#         iterator = FrAngelRandomIterator(deepcopy(g), :Num, rules_min, symbol_min, max_depth=config.generation.max_size)
+#         solution = frangel(spec, config, Dict{UInt16, UInt8}(), iterator, rules_min, symbol_min)
+#     end
+#     program = rulenode2expr(solution, g)
+#     println(program)
 
-    @time begin
-        iterator = BFSIterator(g, :Num, max_depth=10)
-        solution, flag = synth(problem, iterator)
-    end
-    program = rulenode2expr(solution, g)
-    println(program)
-end
+#     @time begin
+#         iterator = BFSIterator(g, :Num, max_depth=10)
+#         solution, flag = synth(problem, iterator)
+#     end
+#     program = rulenode2expr(solution, g)
+#     println(program)
+# end
 
 @testset "basic_example_with_angelic" begin
     spec = [
@@ -49,8 +49,8 @@ end
     config = FrAngelConfig(max_time=20, verbose_level=0,
         generation=FrAngelConfigGeneration(use_fragments_chance=0.5, use_angelic_conditions_chance=0.75, max_size=20),
         angelic=FrAngelConfigAngelic(max_allowed_fails=0.5, max_execute_attempts=5))
-    angelic_conditions = Dict{UInt16,UInt8}()
-    angelic_conditions[18] = 1
+    angelic_conditions = Dict{UInt16,UInt8}(18 => 1)
+
     @time begin
         # @time @profview begin     
         iterator = FrAngelRandomIterator(deepcopy(g), :Num, rules_min, symbol_min, max_depth=config.generation.max_size)
@@ -67,26 +67,24 @@ end
     println(program)
 end
 
-@testset "getRange" begin
-    grammar = buildProgrammingProblemGrammar([(:endd, :Num), (:start, :Num)], :List)
-    spec = [
-        IOExample(Dict(:start => 10, :endd => 15), [10, 11, 12, 13, 14]),
-        IOExample(Dict(:start => 10, :endd => 11), [10]),
-        IOExample(Dict(:start => 0, :endd => 1), [0]),
-    ]
-    problem = Problem(spec)
+# @testset "getRange" begin
+#     grammar = buildProgrammingProblemGrammar([(:endd, :Num), (:start, :Num)], :List)
+#     spec = [
+#         IOExample(Dict(:start => 10, :endd => 15), [10, 11, 12, 13, 14]),
+#         IOExample(Dict(:start => 10, :endd => 11), [10]),
+#         IOExample(Dict(:start => 0, :endd => 1), [0]),
+#     ]
+#     problem = Problem(spec)
+#     angelic_conditions = Dict{UInt16,UInt8}(6 => 1, 7 => 1)
 
-    angelic_conditions = Dict{UInt16, UInt8}()
-    angelic_conditions[6] = 1
-    angelic_conditions[7] = 1
-    config = FrAngelConfig(max_time=10, generation=FrAngelConfigGeneration(use_fragments_chance=0.5, use_angelic_conditions_chance=0.5))
+#     config = FrAngelConfig(max_time=10, generation=FrAngelConfigGeneration(use_fragments_chance=0.5, use_angelic_conditions_chance=0.5))
 
-    rules_min = rules_minsize(grammar)
-    symbol_min = symbols_minsize(grammar, rules_min)
-    @time begin
-        iterator = FrAngelRandomIterator(grammar, :Program, rules_min, symbol_min, max_depth=config.generation.max_size)
-        solution = frangel(spec, config, angelic_conditions, iterator, rules_min, symbol_min)
-    end
-    program = rulenode2expr(solution, grammar)
-    println(program)
-end
+#     rules_min = rules_minsize(grammar)
+#     symbol_min = symbols_minsize(grammar, rules_min)
+#     @time begin
+#         iterator = FrAngelRandomIterator(grammar, :Program, rules_min, symbol_min, max_depth=config.generation.max_size)
+#         solution = frangel(spec, config, angelic_conditions, iterator, rules_min, symbol_min)
+#     end
+#     program = rulenode2expr(solution, grammar)
+#     println(program)
+# end
