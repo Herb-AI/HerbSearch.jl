28964a8ba47d61ba6ff9a5084e9f1771a5c12c80
Nils Marten Mikk <nilsmikk@gmail.com>
2024-06-19 16:01:47 +0200

    Increase number of runs to 10

--- run_experiment.sh
+++ run_experiment.sh
@@ -2,5 +2,5 @@
 
 for seed in 958129 95812 11248956 6354 999999
 do
-    julia --project=. src/minecraft/benchmark.jl -e $1 -s $seed -t 3 --max-time 1200
+    julia --project=. src/minecraft/benchmark.jl -e $1 -s $seed -t 10 --max-time 1200
 done
