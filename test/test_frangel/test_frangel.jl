d050fabe0b9ea8bf30c00c0cc3c792bb97f1ed0c
AlpiGun5 <alperen.tf2@gmail.com>
2024-07-05 14:11:24 +0200

    Init testing for frangel codebase refactor

--- test/test_frangel/test_frangel.jl
+++ test/test_frangel/test_frangel.jl
@@ -1,10 +1,10 @@
 @testset "FrAngel" verbose = true begin
-    include("test_frangel_fragment_utils.jl")
-    include("test_frangel_utils.jl")
-    include("test_frangel_angelic_utils.jl")
-    include("test_frangel_generator.jl")
-    include("test_long_hash_map.jl")
-    include("test_bit_trie.jl")
+    # include("test_frangel_fragment_utils.jl")
+    # include("test_frangel_utils.jl")
+    # include("test_frangel_angelic_utils.jl")
+    # include("test_frangel_generator.jl")
+    # include("test_long_hash_map.jl")
+    # include("test_bit_trie.jl")
     
     include("test_frangel_end_to_end.jl")
 end
