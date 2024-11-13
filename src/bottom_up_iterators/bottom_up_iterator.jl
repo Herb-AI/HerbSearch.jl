b19323cd5fec6927409d3b620c8cafa369c51bda
TudorMagirescu <tudor.magirescu@gmail.com>
2024-11-13 22:53:20 +0100

    Made observational equivalence an optional optimization instead of it being enabled by default.

--- src/bottom_up_iterators/bottom_up_iterator.jl
+++ src/bottom_up_iterators/bottom_up_iterator.jl
@@ -2,7 +2,6 @@
 	abstract type BottomUpIterator <: ProgramIterator
 
 Enumerates programs in a bottom-up fashion. This means that complete programs are generated based on a user-provided order (e.g. depth, size, other cost function).
-Observational equivalence (i.e. removing programs which yield the same outputs on all examples) is enabled by default.
 
 Concrete implementations of this iterator should design the following custom data structures:
 - `bank::BottomUpBank`: Meant to store the list of already-generated programs.
@@ -13,6 +12,11 @@ Additionally, the following functions should be implemented:
 - `init_data(iter::BottomUpIterator)::BottomUpData`: Returns an initialized `BottomUpData`.
 - `create_program!(iter::BottomUpIterator, bank::BottomUpBank, data::BottomUpData)::Union{Nothing, RuleNode}`: Generates the next program. Returns `nothing` if no other programs can be generated.
 - `update_state!(iter::BottomUpIterator, bank::BottomUpBank, data::BottomUpData, program::RuleNode)::Nothing`: Called after a program is validated.
+
+Observational equivalence (i.e. removing programs which yield the same outputs on all examples) is an optional optimization which could be used by `BottomUpIterator`s.
+To enable it, add the following fields to the `BottomUpIterator` implementation:
+- `problem::Union{Nothing, Problem{Vector{IOExample}}}.
+- `obs_equivalence::Bool` (and set this to `true` when creating the `BottomUpIterator`).
 """
 abstract type BottomUpIterator <: ProgramIterator end
 
@@ -34,12 +38,13 @@ abstract type BottomUpData end
     mutable struct BottomUpState
 
 Structure defining the internal state of the iterator. Contains the user-defined `bank::BottomUpBank` and `data::BottomUpData`.
-Additionally, contains `hash_set::Set{UInt64}` used for observational equivalence.
+Additionally, contains `enable_observational_equivalence::Bool`, which specifies if the observational equivalence optimization is enabled and `observational_equivalence_hashes::Set{UInt64}` used for storing the hashes of the generated programs.
 """
 mutable struct BottomUpState
     bank::BottomUpBank
     data::BottomUpData
-    hash_set::Set{UInt64}
+    enable_observational_equivalence::Bool
+    observational_equivalence_hashes::Set{UInt64}
 end
 
 """
@@ -48,7 +53,7 @@ end
 Defines the first iteration of the `BottomUpIterator`. Creates the `BottomUpState` by instantiating its components. Calls `_get_next_program` for generating the next program.
 """
 function Base.iterate(iter::BottomUpIterator)::Union{Nothing,Tuple{RuleNode,BottomUpState}}
-    state::BottomUpState = BottomUpState(init_bank(iter), init_data(iter), Set{UInt64}())
+    state::BottomUpState = BottomUpState(init_bank(iter), init_data(iter), _has_obs_equivalence(iter), Set{UInt64}())
     return _get_next_program(iter, state)
 end
 
@@ -65,11 +70,31 @@ function Base.iterate(
 end
 
 """
+    _has_obs_equivalence(iter::BottomUpIterator)::Bool
+
+Returns `true` if the observational equivalence is enabled in the provided `BottomUpIterator` and `false` otherwise.
+"""
+function _has_obs_equivalence(iter::BottomUpIterator)::Bool
+    fields = fieldnames(typeof(iter))
+    has_obs_equivalence_field = :obs_equivalence ∈ fields
+    has_problem_field = :problem ∈ fields
+
+    if !has_obs_equivalence_field
+        return false
+    end
+
+    @assert has_problem_field "A BottomUpIterator containing the 'obs_equivalence' field must have a 'problem' field."
+    @assert !iter.obs_equivalence || !isnothing(iter.problem) "If `obs_equivalence` is set to true, a 'Problem' instance should be provided."
+
+    return iter.obs_equivalence
+end
+
+"""
     function _get_next_program(iter::BottomUpIterator, state::BottomUpState)::Union{Nothing,Tuple{RuleNode,BottomUpState}}
 
 Generates and returns the next program. Internally uses the iterator's `create_program!` function to generate programs, which are validated as follows:
 1. The program shouldn't exceed the depth limit.
-2. The program shouldn't be observationally equivalent to a previous program.
+2. If observational equivalence is enabled, ensures the program isn't observationally equivalent to a previous program.
 3. The program shouldn't violate any constraints. Note that the `update_state!` function is still called even when the constraints aren't satisfied.
 """
 function _get_next_program(
@@ -79,16 +104,17 @@ function _get_next_program(
     program::Union{Nothing, RuleNode} = create_program!(iter, state.bank, state.data)
 
     while program !== nothing
-        # TODO: optimize this to avoid calling depth for all programs.
-        if depth(program) <= get_max_depth(iter.solver) && !_contains_equivalent!(iter, state, program)
-            update_state!(iter, state.bank, state.data, program)
-
-            new_state!(iter.solver, program)
-            if isfeasible(iter.solver)
-                return program, state
+        if depth(program) <= get_max_depth(iter.solver)
+            # Check if an observationally equivalent program was found.
+            if !state.enable_observational_equivalence || !_contains_equivalent!(iter, state, program)
+                update_state!(iter, state.bank, state.data, program)
+    
+                new_state!(iter.solver, program)
+                if isfeasible(iter.solver)
+                    return program, state
+                end
             end
         end
-
         program = create_program!(iter, state.bank, state.data)
     end
     
@@ -98,7 +124,7 @@ end
 """
 	_contains_equivalent(iter::BottomUpIterator, state::BottomUpState, program::RuleNode)::Bool
 
-Checks whether `program` is observationally equivalent to any previously-iterated program. If it is unique, its hash is pushed to `state.hash_set`.
+Checks whether `program` is observationally equivalent to any previously-iterated program. If it isn't, its hash is pushed to `state.observational_equivalence_hashes`.
 """
 function _contains_equivalent!(
 	iter::BottomUpIterator,
@@ -111,10 +137,10 @@ function _contains_equivalent!(
     output = map(example -> execute_on_input(SymbolTable(grammar), rulenode2expr(program, grammar), example.in), problem.spec)
     hashed_output = hash(output)
 
-	if hashed_output ∈ state.hash_set
+	if hashed_output ∈ state.observational_equivalence_hashes
 		return true
 	end
 
-    push!(state.hash_set, hashed_output)
+    push!(state.observational_equivalence_hashes, hashed_output)
 	return false
 end
