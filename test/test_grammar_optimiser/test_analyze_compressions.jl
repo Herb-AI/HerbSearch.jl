4dab96246e022c9f549981f3ae5c2bb9087433e8
Pallabi <p.s.p.sarker@student.tudelft.nl>
2024-11-15 14:36:01 +0100

    test generate stats

--- test/test_grammar_optimiser/test_analyze_compressions.jl
+++ test/test_grammar_optimiser/test_analyze_compressions.jl
@@ -61,3 +61,20 @@ end
     stats_zipped = Dict(RuleNode(1) => (size = 1, occurences = 2), RuleNode(2) => (size = 2, occurences = 4), RuleNode(3) => (size = 3, occurences = 6))
     @test zip_stats(stats) == stats_zipped
 end
+
+@testset verbose=true "Generate Stats" begin
+    Subtree_dict = Dict{Int64, NamedTuple{(:comp_id, :parent_id, :child_nr, :type, :children), <:Tuple{Int64,Int64,Int64,Int64,Vector}}}(
+        2 => (comp_id = 2, parent_id = -1, child_nr = -1, type = 2, children = [3, 5]),
+        3 => (comp_id = 2, parent_id = 2, child_nr = 1, type = 2, children = []),
+        5 => (comp_id = 2, parent_id = 2, child_nr = 2, type = 4, children = []),
+        7 => (comp_id = 7, parent_id = -1, child_nr = -1, type = 0, children = [8,9]),
+        8 => (comp_id = 7, parent_id = 7, child_nr = 0, type = 1, children = []),
+        9 => (comp_id = 7, parent_id = 7, child_nr = 1, type = 1, children = []),
+    )
+    c_ast = ["assign(2, x)", "assign(3, x)", "assign(5, x)", "assign(8, x)", "assign(9, x)", "assign(7, x)", "assign(8, x)", "assign(9, x)", "assign(7, x)"]
+    c_info = Dict{Int64, NamedTuple{(:size, :occurences), <:Tuple{Int64, Int64}}}(
+        7 => (size = 3, occurences = 2), 
+        2 => (size = 3, occurences = 1)
+    )
+    @test generate_stats(Subtree_dict, c_ast) == c_info
+end
