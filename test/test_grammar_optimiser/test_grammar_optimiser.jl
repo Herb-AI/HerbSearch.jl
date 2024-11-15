cad5096b221dddd86ad9d33fd055373e6b5a9c25
Pallabi <p.s.p.sarker@student.tudelft.nl>
2024-11-15 12:24:00 +0100

    cleaned up test code

--- test/test_grammar_optimiser/test_grammar_optimiser.jl
+++ test/test_grammar_optimiser/test_grammar_optimiser.jl
@@ -2,22 +2,18 @@ using Test, HerbCore, HerbGrammar, HerbConstraints
 #Cannot use "using HerbSearch" because HerbSearch does not expose this functionality. 
 include("../../src/grammar_optimiser/grammar_optimiser.jl") 
 
-
-
-# hole = Hole(get_domain(dummy_grammar, g.bytype[:Int]))
-
-dummy_ast1 = RuleNode(2, [RuleNode(1), RuleNode(1)])
-dummy_ast2 = RuleNode(3, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(1), RuleNode(1)])])
-dummy_asts = [dummy_ast1, dummy_ast2]
+ast1 = RuleNode(2, [RuleNode(1), RuleNode(1)])
+ast2 = RuleNode(3, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(1), RuleNode(1)])])
+asts = [ast1, ast2]
 
 @testset verbose=false "Integration Test: Grammar Optimiser, 3->4 Rules" begin
     # Test Values
-    dummy_grammar = @csgrammar begin
+    g = @csgrammar begin
         Int = 1
         Int = Int + Int
         Int = Int * Int
     end
-    optimised_grammar = grammar_optimiser(dummy_asts, dummy_grammar, 1, 0.5, 0)
+    optimised_grammar = grammar_optimiser(asts, g, 1, 0.5, 0)
 
     # Test whether the optimised grammar has the correct number of rules (4) 
     @test length(optimised_grammar.rules) == 4
@@ -30,13 +26,13 @@ end
 
 @testset verbose=false "Integration Test: Grammar Optimiser, No rules added" begin
     # Test Values
-    dummy_grammar = @csgrammar begin
+    g = @csgrammar begin
         Int = 1
         Int = Int + Int
         Int = Int * Int
     end
-    @test length(dummy_grammar.rules) == 3
-    optimised_grammar = grammar_optimiser([RuleNode(1)], dummy_grammar, 1, 0.5, 0)
+    @test length(g.rules) == 3
+    optimised_grammar = grammar_optimiser([RuleNode(1)], g, 1, 0.5, 0)
 
     # Test whether the optimised grammar has the correct number of rules (3) 
     @test length(optimised_grammar.rules) == 3
