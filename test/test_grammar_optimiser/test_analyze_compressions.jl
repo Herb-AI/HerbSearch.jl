d64269188511852ca0833f0296ec6498945eb878
Pallabi <p.s.p.sarker@student.tudelft.nl>
2024-11-15 14:30:41 +0100

    all tests analyze compressions EXCEPT `generate_stats`

--- test/test_grammar_optimiser/test_analyze_compressions.jl
+++ test/test_grammar_optimiser/test_analyze_compressions.jl
@@ -10,14 +10,54 @@ end
 
 ast_1 = RuleNode(1)
 ast_1_duplicate = RuleNode(1)
-ast_2 = RuleNode(2, [RuleNode(1), RuleNode(1)])
+
+ast_2           = RuleNode(2, [RuleNode(1), RuleNode(1)])
 ast_2_duplicate = RuleNode(2, [RuleNode(1), RuleNode(1)])
-ast_3 = RuleNode(3, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(1), RuleNode(1)])])
+
+ast_3           = RuleNode(3, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(1), RuleNode(1)])])
 ast_3_duplicate = RuleNode(3, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(1), RuleNode(1)])])
 
-@testset verbose=false "compare c₁, c₂" begin
+@testset verbose=false "Compare c₁, c₂" begin
     @test compare(ast_1, ast_1_duplicate)
     @test compare(ast_2, ast_2_duplicate)
     @test compare(ast_3, ast_3_duplicate)
     @test compare(ast_1, ast_1)
+end
+
+@testset verbose=false "Select compressions" begin
+    # Make dictionary with RuleNode(1)=>(size=3, occurences=4), RuleNode(2, [RuleNode(1), RuleNode(1)])=>(size=1, occurences=1)
+    c = Dict{RuleNode, NamedTuple{(:size,:occurences), <:Tuple{Int64,Int64}}}(
+        ast_1 => (size = 1, occurences = 4),
+        ast_2 => (size = 3, occurences = 3),
+        ast_3 => (size = 7, occurences = 2))
+
+    @test select_compressions(1, c, 0) == []
+    @test select_compressions(2, c, 1) == [ast_3, ast_2]
+    @test select_compressions(1, c, 1) == [ast_2, ast_3]
+    @test select_compressions(2, c, 0.5) == [ast_3]
+    @test select_compressions(1, c, 0.5) == [ast_2]
+end
+
+@testset verbose=false "Get compression size" begin
+    Subtree_dict = Dict{Int64, NamedTuple{(:comp_id, :parent_id, :child_nr, :type, :children), <:Tuple{Int64,Int64,Int64,Int64,Vector}}}(
+        2 => (comp_id = 2, parent_id = -1, child_nr = -1, type = 2, children = [3, 5]),
+        3 => (comp_id = 2, parent_id = 2, child_nr = 1, type = 2, children = []),
+        5 => (comp_id = 2, parent_id = 2, child_nr = 2, type = 4, children = []),
+        7 => (comp_id = 7, parent_id = -1, child_nr = -1, type = 0, children = [8,9]),
+        8 => (comp_id = 7, parent_id = 7, child_nr = 0, type = 1, children = []),
+        9 => (comp_id = 7, parent_id = 7, child_nr = 1, type = 1, children = []),
+    )
+
+    @test getCompressionSize(Subtree_dict, 2) == 3
+    @test getCompressionSize(Subtree_dict, 7) == 3
+    @test getCompressionSize(Subtree_dict, 1) == 0
+end
+
+@testset verbose=true "Zip Stats" begin
+    stats = Vector{Dict{RuleNode, NamedTuple{(:size,:occurences), <:Tuple{Int64,Int64}}}}()
+    push!(stats, Dict(RuleNode(1) => (size = 1, occurences = 1), RuleNode(2) => (size = 2, occurences = 2)))
+    push!(stats, Dict(RuleNode(2) => (size = 2, occurences = 2), RuleNode(3) => (size = 3, occurences = 3)))
+    push!(stats, Dict(RuleNode(3) => (size = 3, occurences = 3), RuleNode(1) => (size = 1, occurences = 1)))
+    stats_zipped = Dict(RuleNode(1) => (size = 1, occurences = 2), RuleNode(2) => (size = 2, occurences = 4), RuleNode(3) => (size = 3, occurences = 6))
+    @test zip_stats(stats) == stats_zipped
 end
