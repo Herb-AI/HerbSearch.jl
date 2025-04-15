68fdd3ef095a35698066309a2bff5da6dac8b8f9
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2025-04-15 07:55:53 +0200

    Re-enable all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,23 +13,23 @@ using Random
 Random.seed!(1234)
 
 @testset "HerbSearch.jl" verbose = true begin
-	# @testset "Aqua" Aqua.test_all(
-	# 	HerbSearch,
-	# 	piracies = (treat_as_own = [RuleNode, AbstractGrammar],),
-	# )
-	# include("test_search_procedure.jl")
-	# include("test_context_free_iterators.jl")
-	# include("test_sampling.jl")
-	# include("test_stochastic/test_stochastic.jl")
-	# include("test_genetic.jl")
-	# include("test_programiterator_macro.jl")
-	# include("test_uniform_iterator.jl")
-	# include("test_forbidden.jl")
-	# include("test_ordered.jl")
-	# include("test_contains.jl")
-	# include("test_contains_subtree.jl")
-	# include("test_unique.jl")
-	# include("test_constraints.jl")
+	@testset "Aqua" Aqua.test_all(
+		HerbSearch,
+		piracies = (treat_as_own = [RuleNode, AbstractGrammar],),
+	)
+	include("test_search_procedure.jl")
+	include("test_context_free_iterators.jl")
+	include("test_sampling.jl")
+	include("test_stochastic/test_stochastic.jl")
+	include("test_genetic.jl")
+	include("test_programiterator_macro.jl")
+	include("test_uniform_iterator.jl")
+	include("test_forbidden.jl")
+	include("test_ordered.jl")
+	include("test_contains.jl")
+	include("test_contains_subtree.jl")
+	include("test_unique.jl")
+	include("test_constraints.jl")
 	include("test_bottom_up.jl")
 
 	# 	# Excluded because it contains long tests
