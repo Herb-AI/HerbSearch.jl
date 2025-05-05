9f6a733ff334d96991a5711631652834f7570379
Stef Rasing <rasing.stef@gmail.com>
2025-05-05 10:56:44 +0200

    Update test.jl
    Update test.jl

--- ext/RefactorExt/test.jl
+++ ext/RefactorExt/test.jl
@@ -2,7 +2,8 @@ using Markdown
 using InteractiveUtils
 include("RefactorExt.jl")
 using .RefactorExt
-#using HerbCore, HerbGrammar, .HerbSearch
+include("../../src/HerbSearch.jl")
+using HerbCore, HerbGrammar, .HerbSearch
 
 
 # Define simple grammar
@@ -10,17 +11,46 @@ grammar = @csgrammar begin
     Int = 1
     Int = Int + Int
     Int = Int * Int
+    Int = Int - Int
+    Int = Int / Int
 end
 
 
 # Define two simple programs that are deemed to be useful
 # Program 1: 1 + 1
-# Program 2: (1 + 1) * (1 + 1)
+# Program 2: ((1 + 1) * (1 + 1)) + ((1 / 1) * (1 + 1))
 ast1 = RuleNode(2, [RuleNode(1), RuleNode(1)])
-ast2 = RuleNode(3, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(1), RuleNode(1)])])
-useful_asts = [ast1, ast2]
+ast2 = RuleNode(2, [
+    RuleNode(3, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(1), RuleNode(1)])]),
+    RuleNode(3, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(5, [RuleNode(1), RuleNode(1)])])
+])
+useful_asts = [ast2, ast1]
 
+
+# Program 1: (1 + 1) + ((1 - 1) + (1 - 1)) -> (1 + 1) + _, (1 - 1) + (1 - 1)
+# Program 2: (1 + 1) + ((1 * 1) + (1 * 1)) -> (1 + 1) + _, (1 * 1) + (1 * 1)
+# Program 3: (1 + 1) + ((1 / 1) + (1 / 1)) -> (1 + 1) + _, (1 / 1) + (1 / 1)
+#ast1 = RuleNode(2, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(3, [RuleNode(1), RuleNode(1)]),RuleNode(3, [RuleNode(1), RuleNode(1)])])])
+#ast2 = RuleNode(2, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(4, [RuleNode(1), RuleNode(1)]),RuleNode(4, [RuleNode(1), RuleNode(1)])])])
+#ast3 = RuleNode(2, [RuleNode(2, [RuleNode(1), RuleNode(1)]), RuleNode(2, [RuleNode(5, [RuleNode(1), RuleNode(1)]),RuleNode(5, [RuleNode(1), RuleNode(1)])])])
+#useful_asts = [ast1, ast2, ast3]
 
 # Optimize grammar by substituting 1 + 1 as a new grammar rule
 optimised_grammar = RefactorExt.HerbSearch.refactor_grammar(useful_asts, grammar, RefactorExt.SelectionStrategy(0), 0.5)
-println("Optimised Grammar: ", optimised_grammar)
+println("Optimised Grammar: ")
+println(optimised_grammar)
+
+#subtrees = vcat(Vector{Any}(), RefactorExt.enumerate_subtrees(ast1, grammar))
+#res = RefactorExt.convert_subtrees_to_json(subtrees, ast1)
+#println(RefactorExt.parse_json(res))
+
+#=
+
+Subtrees of 1 + 1:
+- _ + _
+- 1 + _
+- _ + 1
+_ 1 + 1
+_ 1
+
+=#
