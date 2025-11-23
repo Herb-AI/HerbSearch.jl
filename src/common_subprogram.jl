ac6a35ecfd1d8ded212121e0c8514c0f32929ec8
Alexander <alexander.d.jelev@gmail.com>
2025-11-23 17:17:31 +0100

    added updater and selector functions (unimplemented).

---
+++ src/common_subprogram.jl
@@ -0,0 +1,9 @@
+using ..BudgetedSearch
+
+function selector(results::Vector{Any})
+  
+end
+
+function updater(results::Vector{Any}, iterator::ProgramIterator, grammar::ContextSensitiveGrammar)
+
+end
