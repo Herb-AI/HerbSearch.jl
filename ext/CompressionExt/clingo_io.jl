3006311a4e1ea33897638008234838d75ab2845a
danilBren <danilbren1@gmail.com>
2026-03-03 15:22:30 +0100

    remove logging

--- ext/CompressionExt/clingo_io.jl
+++ ext/CompressionExt/clingo_io.jl
@@ -86,13 +86,11 @@ function read_last_witness_from_json(json_content)
         return nothing
     end
     optimal = json_parsed["Result"] == "OPTIMUM FOUND"
-    @info "Compression optimality: $optimal"
 
     witnesses = json_parsed["Call"][1]["Witnesses"]
 
     last_witness = witnesses[end]
     last_value = last_witness["Value"] #The best solution found
-    @info "Compression result: $last_value"
     return last_value
 end
 
